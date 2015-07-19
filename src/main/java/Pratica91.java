1d7abb0c7f4ca76cd75dc67cddd5eb6bf58c2701
PedroEMSilva <pemanuel2@hotmail.com>
2015-07-19 18:34:56 -0300

    First

--- src/main/java/Pratica91.java
+++ src/main/java/Pratica91.java
@@ -12,7 +12,7 @@ public class Pratica91 {
         Runtime rt = Runtime.getRuntime();
         System.out.println(rt.availableProcessors());
         System.out.println(rt.totalMemory()/(1024*1024));
-        System.out.println(rt.freeMemory()/(1024*1024));
+        System.out.println((rt.freeMemory()/(1024*1024))-1);
         System.out.println(rt.maxMemory()/(1024*1024));
     }
 }
